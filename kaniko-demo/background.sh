35ae18d2d56ab7288f32c7b960dfad288e0e4944
Pablo Lopez <plopez@xebia.fr>
2022-01-17 14:53:09 +0100

    Kaniko

--- kaniko-demo/background.sh
+++ kaniko-demo/background.sh
@@ -3,4 +3,4 @@ docker pull alpine
 docker pull docker
 docker pull plopezfr/friends-quotes:1.0
 docker pull gcr.io/kaniko-project/executor:latest
-docker pull docker-registry
+docker pull registry:2.6.2
