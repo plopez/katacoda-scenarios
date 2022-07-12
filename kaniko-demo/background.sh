a70d74c423a967369e6799711c8a2c543ae7c7ae
Jérôme Masson <sphinxgaia@jeromemasson.fr>
2022-07-12 11:40:18 +0200

    update background to killercode k8s architecture

--- kaniko-demo/background.sh
+++ kaniko-demo/background.sh
@@ -1,6 +1,31 @@
 #!/usr/bin/env bash
+
+# dl image on controlplane
+
 docker pull alpine
 docker pull docker
 docker pull plopezfr/friends-quotes:1.0
 docker pull gcr.io/kaniko-project/executor:latest
 docker pull registry:2.6.2
+
+crictl pull alpine
+crictl pull docker
+crictl pull plopezfr/friends-quotes:1.0
+crictl pull gcr.io/kaniko-project/executor:latest
+crictl pull registry:2.6.2
+
+# dl image on node01
+
+ssh node01 '
+docker pull alpine
+docker pull docker
+docker pull plopezfr/friends-quotes:1.0
+docker pull gcr.io/kaniko-project/executor:latest
+docker pull registry:2.6.2
+
+crictl pull alpine
+crictl pull docker
+crictl pull plopezfr/friends-quotes:1.0
+crictl pull gcr.io/kaniko-project/executor:latest
+crictl pull registry:2.6.2
+'
